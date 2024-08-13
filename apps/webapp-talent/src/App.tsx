045689c69e98461d3a49c9b838e6a88bd9473919
Art Mashanin <artjom.mashanin@gmail.com>
2024-08-13 23:32:49 +0300

    feat: setup custom taildwind style autosuggest

--- apps/webapp-talent/src/App.tsx
+++ apps/webapp-talent/src/App.tsx
@@ -1,13 +1,12 @@
-// import "@repo/ui/globals.css";
-import "@adart/webapp-shared/globals.css";
+import "@repo/ui/globals.css";
+import "./App.css";
 
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import { Button } from "@repo/ui/components/ui/button";
 // import { Skeleton } from "@repo/ui/components/ui/skeleton";
 // import { Button } from "@repo/ui/components/ui/button";
-import { Button } from "@adart/webapp-shared/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,7 +14,7 @@ function App() {
   return (
     <>
       <div>
-        <Button>CLICK ME</Button>
+        <Button className="bg-primary-500">CLICK ME</Button>
 
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
